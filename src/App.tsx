6c222622fbbcb7d7501d6990cb159a1c59a680f0
franklin <franklinjunior021118@gmail.com>
2024-07-08 17:24:17 -0500

    chore: Add configuration files and dependencies for project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { ThemeProvider } from './providers/theme.provider'
+
+function App() {
+    return (
+        <>
+            <ThemeProvider>
+                holadwa awdawd (awdawda daw dadw awdawddaw d)
+            </ThemeProvider>
+        </>
+    )
+}
+
+export default App
