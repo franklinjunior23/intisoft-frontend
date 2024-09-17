460155f2586f53728d90eeb0a294bceba9b1ff77
frankjunior23 <franklinjunior021118@gmail.con>
2024-09-17 16:21:41 -0500

    avamce 17

--- src/helper/axios-config.ts
+++ src/helper/axios-config.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios'
 
 const { MODE, VITE_API_DOMAIN, VITE_API_DOMAIN_PRODUCTION } = import.meta.env
-
+console.log('MODE', MODE)
 export const InstanceAxios: AxiosInstance = axios.create({
     baseURL:
         MODE === 'development' ? VITE_API_DOMAIN : 'http://localhost:4000/api/v1/',
